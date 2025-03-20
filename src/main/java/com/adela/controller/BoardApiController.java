0fb721eb2b6c54c333875c3781857ec767c953c7
junhaha1 <wnsgk919@naver.com>
2025-03-20 23:55:20 +0900

    Board Api Controller init

---
+++ src/main/java/com/adela/controller/BoardApiController.java
@@ -0,0 +1,11 @@
+package com.adela.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequiredArgsConstructor
+@RestController
+public class BoardApiController {
+
+}
