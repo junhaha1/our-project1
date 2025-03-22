139f21fac228dd95bc5fed7ce1619c4085c13c27
junhaha1 <wnsgk919@naver.com>
2025-03-23 00:23:02 +0900

    BoardApi Common Setup

--- src/main/java/com/adela/controller/BoardApiController.java
+++ src/main/java/com/adela/controller/BoardApiController.java
@@ -1,5 +1,6 @@
 package com.adela.controller;
 
+import com.adela.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class BoardApiController {
+
+    private final BoardService boardService;
 
 }
