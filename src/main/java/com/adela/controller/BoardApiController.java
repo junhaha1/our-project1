3dd5422e854c5bd48181cd6995e503f47694a188
junhaha1 <wnsgk919@naver.com>
2025-03-23 03:03:09 +0900

    상세 조회, 게시글 수정, 게시글 삭제 API 구현

--- src/main/java/com/adela/controller/BoardApiController.java
+++ src/main/java/com/adela/controller/BoardApiController.java
@@ -1,8 +1,12 @@
 package com.adela.controller;
 
+import com.adela.domain.Article;
+import com.adela.dto.ArticleResponse;
+import com.adela.dto.UpdateArticleRequest;
 import com.adela.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RequiredArgsConstructor
@@ -11,4 +15,27 @@ public class BoardApiController {
 
     private final BoardService boardService;
 
+    @GetMapping("/board/list/{articleId}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable("articleId") long id){
+        Article article = boardService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    @DeleteMapping("/board/article/{articleId}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable("articleId") long id){
+        boardService.delete(id);
+
+        return ResponseEntity.ok()
+                .build();
+    }
+
+    @PutMapping("/board/article/{articleId}")
+    public ResponseEntity<Article> updateArticle(@PathVariable("articleId") long id, @RequestBody UpdateArticleRequest request){
+        Article updateArticle = boardService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updateArticle);
+    }
 }
--- src/main/java/com/adela/service/BoardService.java
+++ src/main/java/com/adela/service/BoardService.java
@@ -1,11 +1,37 @@
 package com.adela.service;
 
+import com.adela.domain.Article;
+import com.adela.dto.UpdateArticleRequest;
 import com.adela.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @RequiredArgsConstructor
 @Service
 public class BoardService {
     private final BoardRepository boardRepository;
+
+    //게시판 ID를 이용하여 해당 게시글 상세조회하기
+    public Article findById(long id){
+        return boardRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    //게시글 ID로 삭제
+    public void delete(long id){
+        boardRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request){
+        Article article = boardRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent(), request.getCodeContent(), request.getErrorContent(), LocalDate.now());
+
+        return article;
+    }
 }
